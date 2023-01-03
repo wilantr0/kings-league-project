2a7c8ce4fb53f66def995f6ae0e8fc2db1a18ba4
Guillermo <guilleballarin19@gmail.com>
2023-01-03 21:02:05 +0100

    primeros pasos

---
+++ scraping/index.js
@@ -0,0 +1,10 @@
+import * as cheerio from 'cheerio'
+
+const res = await fetch('https://kingsleague.pro/estadisticas/clasificacion/')
+const html = await res.text()
+
+const $ = cheerio.load(html)
+
+$('table tbody tr').each((index, el) => {
+    console.log($(el).text())
+})
